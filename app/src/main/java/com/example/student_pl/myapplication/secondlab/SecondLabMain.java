f428ea479f61b12a6b793528d4f3c9e50d2e40ce
RomanDrohobytskyi <romabikebmx@gmail.com>
2018-10-07 22:15:35 +0200

    Layouts.

--- app/src/main/java/com/example/student_pl/myapplication/secondlab/SecondLabMain.java
+++ app/src/main/java/com/example/student_pl/myapplication/secondlab/SecondLabMain.java
@@ -1,9 +1,13 @@
 package com.example.student_pl.myapplication.secondlab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.student_pl.myapplication.R;
+import com.example.student_pl.myapplication.first.lab.FirstMain;
+import com.example.student_pl.myapplication.secondlab.colors.Colors;
 
 public class SecondLabMain extends AppCompatActivity {
 
@@ -11,5 +15,15 @@ public class SecondLabMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_lab_main);
+    }
+
+    public void rgbColor(View view){
+        Intent intent = new Intent(this, Colors.class);
+        startActivity(intent);
+    }
+
+    public void cmykColor(View view){
+        Intent intent = new Intent(this, Colors.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/student_pl/myapplication/secondlab/colors/Colors.java
@@ -0,0 +1,15 @@
+package com.example.student_pl.myapplication.secondlab.colors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.student_pl.myapplication.R;
+
+public class Colors extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colors);
+    }
+}
